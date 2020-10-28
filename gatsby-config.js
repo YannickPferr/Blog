e5b41883bf1026a86e8cf13c9dec6307708965ba
Yannick Pferr <YannickPfer@github.com>
2020-10-28 15:17:43 +0100

    Switched Starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,50 +1,191 @@
-const dotenv = require("dotenv");
-
-//if (process.env.ENVIRONMENT !== "production") {
-  dotenv.config();
-//}
-
-//const { spaceId, accessToken } = process.env;
-
-const spaceId = process.env.CONTENTFUL_SPACE_ID;
-const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
 
 module.exports = {
   siteMetadata: {
-    title: `Rohit Gupta`,
-    description: `Personal Site`,
-    author: `@rohitguptab`
+    siteUrl: "https://gatsby-starter-voyager.netlify.com",
+    author: "gregdferrell",
+    publisher: "gatsby-starter-voyager.netlify.com",
+    title: "Voyager",
+    description:
+      "Gatsby Starter Voyager is feature-rich starter blog. It's MIT licensed and ready to be used as-is or as a starting point from which to build something tailored to your needs. Use it, learn from it, build on it & enjoy.",
+    image: "/images/logo.jpg",
+    bannerImage: "/images/banner.png",
+    blogPostsPerPage: 5,
+    social: {
+      instagram: "",
+      twitter: "",
+    },
+    mailchimpUrl: "",
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`
-      }
+        name: "posts",
+        path: `${__dirname}/content/copy/blog-posts/`,
+      },
     },
     {
-      resolve: "gatsby-source-contentful",
+      resolve: "gatsby-source-filesystem",
       options: {
-        spaceId,
-		accessToken
-      }
+        name: "pages",
+        path: `${__dirname}/content/copy/pages/`,
+      },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-remark`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `Rohit Gupta`,
-        short_name: `Rohit Gupta`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#333`,
-        icon: `src/images/fev_icon.png` // This path is relative to the root of the site.
-      }
-    }, // To learn more, visit: https://gatsby.dev/offline // this (optional) plugin enables Progressive Web App + Offline functionality
-    `gatsby-plugin-offline`
-  ]
-};
+        name: "images",
+        path: `${__dirname}/content/images/`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "data",
+        path: `${__dirname}/content/data`,
+      },
+    },
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          // gatsby-remark-relative-images must
+          // go before gatsby-remark-images
+          {
+            resolve: `gatsby-remark-relative-images`,
+          },
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 1000,
+              linkImagesToOriginal: false,
+            },
+          },
+          {
+            resolve: "gatsby-remark-external-links",
+            options: {
+              target: "_blank",
+              rel: "noopener",
+            },
+          },
+          {
+            resolve: "gatsby-plugin-catch-links",
+          },
+        ],
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-plugin-feed",
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                  date: edge.node.frontmatter.date,
+                  description: edge.node.frontmatter.excerpt,
+                  guid:
+                    site.siteMetadata.siteUrl +
+                    "/blog/" +
+                    edge.node.fields.slug,
+                  url:
+                    site.siteMetadata.siteUrl +
+                    "/blog/" +
+                    edge.node.fields.slug,
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  filter: { frontmatter: { type: { eq: "post" } } published: { eq: true }}
+                  sort: { order: DESC, fields: [frontmatter___date] }
+                ) {
+                  edges {
+                    node {
+                      fields { slug }
+                      html
+                      frontmatter {
+                        title
+                        author
+                        date
+                        tags
+                        excerpt
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "RSS Feed",
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+    },
+    "gatsby-plugin-robots-txt",
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: "", // Google Analytics Tracking ID
+    //     head: false,
+    //     respectDNT: true,
+    //     cookieDomain: "", // Your Domain
+    //   },
+    // },
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        google: {
+          families: ["Open Sans", "Rock Salt", "Mansalva", "Lily Script One"],
+        },
+      },
+    },
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        // req props
+        name: "Gatsby Starter Voyager",
+        short_name: "Voyager",
+        start_url: "/",
+        background_color: "#fff",
+        theme_color: "#9c7c38",
+        display: "minimal-ui",
+        // optional
+        icon: "static/images/logo.jpg",
+        include_favicon: false,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-offline",
+      options: {
+        precachePages: ["", "/blog", "/about"],
+      },
+    },
+  ],
+}
