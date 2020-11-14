e3a2818c1a686b8cba685d1ce0f7e6784d8151af
Yannick Pferr <YannickPfer@github.com>
2020-11-14 21:04:22 +0100

    Implement crawling changes and font fix

--- src/pages/legal-notice.jsx
+++ src/pages/legal-notice.jsx
@@ -1,12 +1,16 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import Helmet from "react-helmet"
 
 import styles from "./legal-notice.module.scss"
 
-const TermsOfService = ({ data }) => {
+const LegalNotice = ({ data }) => {
   return (
     <Layout title="Legal Notice" pathName="/legal-notice">
+      <Helmet>
+          <meta name="robots" content="noindex"/>
+      </Helmet>
       <h1 className="page-heading">Legal Notice</h1>
       <section className={styles.legalNotice}>
             <div dangerouslySetInnerHTML={{ __html: data.markdown.markdownText.childMarkdownRemark.html}} />
@@ -27,4 +31,4 @@ export const query = graphql`
   }
 `
 
-export default TermsOfService
+export default LegalNotice
--- src/pages/privacy-policy.jsx
+++ src/pages/privacy-policy.jsx
@@ -1,12 +1,16 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import Helmet from "react-helmet"
 
 import styles from "./privacy-policy.module.scss"
 
 const PrivacyPolicy = ({ data }) => {
   return (
     <Layout title="Privacy Policy" pathName="/privacy-policy">
+        <Helmet>
+          <meta name="robots" content="noindex"/>
+        </Helmet>
         <h1 className="page-heading">Privacy Policy</h1>
         <section className={styles.privacyPolicy}>
             <div dangerouslySetInnerHTML={{ __html: data.markdown.markdownText.childMarkdownRemark.html}} />
