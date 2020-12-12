1ecbc338f30103e2eb469a317cd9728275a3c2d2
Yannick Pferr <YannickPfer@github.com>
2020-12-12 13:43:02 +0100

    Add wrap root to gatsby ssr to fix build error

---
+++ gatsby-ssr.js
@@ -0,0 +1,14 @@
+// gatsby-ssr.js
+import React from 'react';
+import { ToastProvider } from 'react-toast-notifications'
+
+export const wrapRootElement = ({ element }) => {
+    return (
+        <ToastProvider
+            autoDismiss
+            autoDismissTimeout={3000}
+            placement="top-center">
+            { element}
+        </ToastProvider >
+    );
+}
