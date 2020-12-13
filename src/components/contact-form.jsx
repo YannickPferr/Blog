0f7257cc50bda91ca725842ed14375d6f9e4ad30
Yannick Pferr <YannickPfer@github.com>
2020-12-13 22:03:12 +0100

    Change contact form url and make fields required

--- src/components/contact-form.jsx
+++ src/components/contact-form.jsx
@@ -5,36 +5,39 @@ import styles from "./contact-form.module.scss"
 const ContactForm = ({ input }) => {
 
     return (
-        <form className={styles.form} action="https://formsubmit.io/send/c59aa336-7221-49a2-8838-2585da01783a" method="post">
+        <form className={styles.form} action="https://formsubmit.io/send/b3442703-db64-49ca-a5fc-0a45feee3efd" method="post">
             <div>
-                <label className="clip" htmlFor="name">Your Name</label>
+                <label className="clip" htmlFor="name">Your Name*</label>
                 <input
                     className={styles.formInput}
-                    placeholder="Your Name"
+                    placeholder="Your Name*"
                     type="text"
                     name="NAME"
                     id="name"
+                    required
                 />
             </div>
             <div>
-                <label className="clip" htmlFor="email">Your Email Address</label>
+                <label className="clip" htmlFor="email">Your Email Address*</label>
                 <input
                     className={styles.formInput}
-                    placeholder="Your Email Address"
+                    placeholder="Your Email Address*"
                     name="EMAIL"
                     id="email"
                     type="email"
+                    required
                 />
             </div>
             <div>
-                <label className="clip" htmlFor="message">Your Message</label>
+                <label className="clip" htmlFor="message">Your Message*</label>
                 <textarea
                     className={styles.formInput}
-                    placeholder="Your Message"
+                    placeholder="Your Message*"
                     name="MESSAGE"
                     id="message"
                     type="text"
                     rows="5"
+                    required
                 />
             </div>
             <input
